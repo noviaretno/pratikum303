1d31b54463216a77f959d0165f956b2b8326ab6d
noviaretno <32433423+noviaretno@users.noreply.github.com>
2017-11-07 07:53:40 -0800

    menghitung gajih bersih

--- 03.cpp
+++ 03.cpp
@@ -10,4 +10,11 @@ int main (){
 	cin >> gp;
 	cout << " Masukkan jam kerja : ";
 	cin >> jk;
+	
+	lembur= jk -40;
+	gt = (lembur*1.5+40)+gp;
+	pajak=gt*0.15;
+	gt=gt-pajak;
+	
+	cout<<"Gajih bersih pegawai : "<< gt<<endl;
 }
